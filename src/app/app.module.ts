4458cf1d078250dee16ac8d7c1904f4e0b5cb982
Jens Faeustl <jens.faeustl@faeustl.de>
2018-05-10 22:36:48 +0200

    e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SecondComponent } from './second/second.component';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatExpansionModule} from '@angular/material/expansion';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AmexioWidgetModule} from 'amexio-ng-extensions';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,8 @@ import { SecondComponent } from './second/second.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,BrowserAnimationsModule, MatToolbarModule, MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
